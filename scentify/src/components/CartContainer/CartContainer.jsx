e79d22aad16a4b84dd2e8e4592155219c205a742
ignaciogonzalezinigo1 <ignaciogonzalez1@uca.edu.ar>
2025-10-15 12:30:49 -0300

    Solucionado: Falta que elimine de a uno y ponerlo item por item en tarjetitas... Ademas quiero dsp agregar el total

--- scentify/src/components/CartContainer/CartContainer.jsx
+++ scentify/src/components/CartContainer/CartContainer.jsx
@@ -8,38 +8,36 @@ function CartContainer() {
 
     // Carrito vacio
     if (cartItems.length === 0) {
-    return (
-        <section style={{ textAlign: "center", padding: "2rem" }}>
-        <h1>Carrito vacío</h1>
-        <p>Agregá productos para comenzar tu compra.</p>
-        <Link to="/">
-            <button>Volver a la tienda</button>
-        </Link>
-        </section>
-    );
+        return (
+            <section style={{ textAlign: "center", padding: "2rem" }}>
+            <h1>Carrito vacío</h1>
+            <p>Agregá productos para comenzar tu compra.</p>
+            <Link to="/">
+                <button>Volver a la tienda</button>
+            </Link>
+            </section>
+        );
     }
 
-  // Carrito con productos
+    // Carrito con productos
     return (
         <section>
             <h1>Carrito</h1>
 
             {cartItems.map((item) => (
-            <div>
-                <img src={item.img} width={80} />
-                <h3>{item.title}</h3>
-                <p>Cantidad: {item.quantity}</p>
-                <p>Precio unitario: ${item.price}</p>
-                <p>Subtotal: ${item.price * item.quantity}</p>
-                <button onClick={() => removeItem(item.id)}>Eliminar</button>
-            </div>
+                <div key={item.id}>
+                    <img src={item.img} width={80} alt={item.title} />
+                    <h3>{item.title}</h3>
+                    <p>Cantidad: {item.quantity}</p>
+                    <p>Precio unitario: ${item.price}</p>
+                    <p>Subtotal: ${item.price * item.quantity}</p>
+                    <button onClick={() => removeItem(item.id)}>Eliminar</button>
+                </div>
             ))}
 
-            <h2>Total a pagar: ${countTotalPrice()}</h2>
+            {/* <h2>Total a pagar: ${countTotalPrice()}</h2> */}
             <button onClick={clearCart}>Vaciar carrito</button>
-            <Link to="/checkout">
             <button>Ir al checkout</button>
-            </Link>
         </section>
     );
 }

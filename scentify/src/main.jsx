fbca3d981e2b598cdac8e51bfea13df2b14be2e1
Ignacio Gonzalez Inigo <ignaciogonzalezinigo@gmail.com>
2025-10-16 14:17:54 -0300

    Pase a mano los prod a la base de datos y ahora falla detalle

--- scentify/src/main.jsx
+++ scentify/src/main.jsx
@@ -1,9 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import { exportProductsToFirestore } from "./data/firebase.js";
 import './index.css';
 import App from './App.jsx';
 
-exportProductsToFirestore(); // Ejecutar SOLO una vez para subir productos
 
 const rootDiv = document.getElementById('root');
 const reactRoot = createRoot(rootDiv);
